090389a88e025a5e9686bcaa5f4ec74c2f58a156
Florian ERNST <florianernst59@gmail.com>
2022-01-23 18:15:38 +0100

    Switched from a JS-based reactive hamburger icon to a CSS-based one

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,21 +2,9 @@ import Link from 'next/link'
 import * as React from 'react'
 import { FaBars } from 'react-icons/fa'
 
-const mediaQuery = '(min-width: 1300px) and (min-height: 300px)'
-
 export const Header: React.FC<{
   setIsBurgerMenuOpen: (cb: (isOpen: boolean) => boolean) => void
 }> = ({ setIsBurgerMenuOpen }) => {
-  const [hasMenuIcon, setHasMenuIcon] = React.useState(
-    typeof window !== 'undefined' && !window.matchMedia(mediaQuery).matches
-  )
-
-  React.useEffect(() => {
-    window.matchMedia(mediaQuery).addEventListener('change', (e) => {
-      setHasMenuIcon(!e.matches)
-    })
-  }, [setHasMenuIcon])
-
   return (
     <header className='notion-header'>
       <div
@@ -27,18 +15,17 @@ export const Header: React.FC<{
           fontSize: '16px'
         }}
       >
-        {hasMenuIcon && (
-          <FaBars
-            onClick={() => setIsBurgerMenuOpen((isOpen) => !isOpen)}
-            id='hamburger-menu-icon'
-            style={{
-              width: '20px',
-              height: '20px',
-              marginRight: '20px',
-              cursor: 'pointer'
-            }}
-          />
-        )}
+        <FaBars
+          onClick={() => setIsBurgerMenuOpen((isOpen) => !isOpen)}
+          id='hamburger-menu-icon'
+          style={{
+            width: '20px',
+            height: '20px',
+            marginRight: '20px',
+            cursor: 'pointer'
+          }}
+        />
+
         <Link href='/'>
           <a
             style={{
