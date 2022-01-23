cb2e89afb397b2848b5c3aacb308d02952844647
Florian ERNST <florianernst59@gmail.com>
2022-01-23 16:12:21 +0100

    Switched to official Notion client for database fetching

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,8 +1,13 @@
+import { Client } from '@notionhq/client'
 import { NotionAPI } from 'notion-client'
 import { SearchParams, SearchResults } from 'notion-types'
 
 export const notion = new NotionAPI({
   apiBaseUrl: process.env.NOTION_API_BASE_URL
+})
+
+export const notionOfficialClient = new Client({
+  auth: process.env.NOTION_API_KEY
 })
 
 export async function search(params: SearchParams): Promise<SearchResults> {
--- lib/resolve-notion-page.ts
+++ lib/resolve-notion-page.ts
@@ -1,85 +1,38 @@
-import { ExtendedRecordMap } from 'notion-types'
-import { parsePageId } from 'notion-utils'
-import * as acl from './acl'
 import { config } from './config'
-import { fetchDatabase } from './fetchDatabase'
 import { getSiteForDomain } from './get-site-for-domain'
 import { getSiteMaps } from './get-site-maps'
-import { notion } from './notion'
-import * as types from './types'
+import { notion, notionOfficialClient } from './notion'
 
 const { pageUrlAdditions, pageUrlOverrides } = config
 
-export async function resolveNotionPage(domain: string, rawPageId?: string) {
-  const site: types.Site = getSiteForDomain(domain)
-  let pageId: string
-  let recordMap: ExtendedRecordMap
-  let sideBar: types.SideBarItems = null
-
+async function getPageId(rawPageId: string | undefined) {
   if (rawPageId && rawPageId !== 'index') {
-    pageId = parsePageId(rawPageId)
-
-    if (!pageId) {
-      // check if the site configuration provides an override of a fallback for
-      // the page's URI
-      const override =
-        pageUrlOverrides[rawPageId] || pageUrlAdditions[rawPageId]
-
-      if (override) {
-        pageId = parsePageId(override)
-      }
-    }
+    const pageId =
+      pageUrlOverrides[rawPageId] ??
+      pageUrlAdditions[rawPageId] ??
+      (await getSiteMaps())[0]?.canonicalPageMap[rawPageId]
 
     if (pageId) {
-      const resources = await Promise.all([
-        notion.getPage(pageId),
-        fetchDatabase(config.pagesDatabaseId)
-      ])
-
-      recordMap = resources[0]
-      sideBar = resources[1]
-    } else {
-      // handle mapping of user-friendly canonical page paths to Notion page IDs
-      // e.g., /developer-x-entrepreneur versus /71201624b204481f862630ea25ce62fe
-      const siteMaps = await getSiteMaps()
-      const siteMap = siteMaps[0]
-      pageId = siteMap?.canonicalPageMap[rawPageId]
-
-      if (pageId) {
-        // TODO: we're not re-using the site from siteMaps because it is
-        // cached aggressively
-        // site = await getSiteForDomain(domain)
-        // recordMap = siteMap.pageMap[pageId]
-        const resources = await Promise.all([
-          notion.getPage(pageId),
-          fetchDatabase(config.pagesDatabaseId)
-        ])
-
-        recordMap = resources[0]
-        sideBar = resources[1]
-      } else {
-        return {
-          error: {
-            message: `Not found "${rawPageId}"`,
-            statusCode: 404
-          }
-        }
-      }
+      return pageId
     }
-  } else {
-    pageId = site.rootNotionPageId
 
-    console.log(site)
-
-    const resources = await Promise.all([
-      notion.getPage(pageId),
-      fetchDatabase(config.pagesDatabaseId)
-    ])
-
-    recordMap = resources[0]
-    sideBar = resources[1]
+    throw new Error(`Could not find pageId for ${rawPageId}`)
   }
 
-  const props = { site, recordMap, pageId, sideBar }
-  return { ...props, ...(await acl.pageAcl(props)) }
+  return config.rootNotionPageId
+}
+
+export async function resolveNotionPage(domain: string, rawPageId?: string) {
+  const site = getSiteForDomain(domain)
+
+  const pageId = await getPageId(rawPageId)
+
+  const [recordMap, sideBar] = await Promise.all([
+    notion.getPage(pageId),
+    notionOfficialClient.databases.retrieve({
+      database_id: config.pagesDatabaseId
+    })
+  ])
+
+  return { site, recordMap, pageId, sideBar }
 }
