bc0db152eb2d1ce62e446c6e4f095d2f76eb7b80
Florian Ernst <florian.ernst@alan.eu>
2022-01-29 11:42:01 +0100

    Added analytics

--- pages/api/cgi.ts
+++ pages/api/cgi.ts
@@ -4,10 +4,14 @@ import fetch from 'node-fetch'
 export default (req: NextApiRequest, res: NextApiResponse) => {
   return fetch('https://static.cloudflareinsights.com/beacon.min.js').then(
     (script) =>
-      script.arrayBuffer().then((data) => {
+      script.text().then((data) => {
         res.setHeader('Content-Type', script.headers.get('Content-Type'))
         res.setHeader('Cache-Control', `public, max-age=${604800}`)
-        res.status(200).send(Buffer.from(data))
+        res
+          .status(200)
+          .send(
+            Buffer.from(data.replace('https://cloudflareinsights.com/', '/'))
+          )
       })
   )
 }
